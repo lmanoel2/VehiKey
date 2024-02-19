43aafa8697b27d5fedd02da05b26444bd76bf87a
Manoel Sousa <manoelvsousa1@gmail.com>
2024-02-19 19:01:46 -0300

    feat: init project

---
+++ Domain/Entities/Vehicle.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime(timezone=True), default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    model = Column(String(20), nullable=True)
+    color = Column(String(10), nullable=True)
+    plate = Column(String(10), nullable=False)
+
+
---
+++ Startup.py
@@ -0,0 +1,3 @@
+print('Startup...')
+
+print('Finish...')
