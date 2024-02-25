66350fcdcac02b0ee4be5d7fb5914a94cc8f3b49
Manoel Sousa <manoelvsousa1@gmail.com>
2024-02-25 18:19:48 -0300

    feat: added unit test to car with plate not valid

--- Tests/ApplicationTests/ServicesTests/RecognitionPlateServiceTests/RecognitionPlateServiceTest.py
+++ Tests/ApplicationTests/ServicesTests/RecognitionPlateServiceTests/RecognitionPlateServiceTest.py
@@ -1,15 +1,16 @@
 import cv2
 import os
+import pytest
 from Application.Services.Recognition.RecognitionPlateService import RecognitionPlateService
 
 parentDirectoryTest = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 
 
-def testGetTextPlateOldFromImage():
+@pytest.mark.parametrize("recognitionPlateService", [RecognitionPlateService()])
+def testGetTextPlateOldFromImage(recognitionPlateService):
     # Arrange
     directoryImage = os.path.join(parentDirectoryTest, 'UtilsTests', 'Photos', 'CarWithOldPlate.jpg')
     image = cv2.imread(directoryImage)
-    recognitionPlateService = RecognitionPlateService()
 
     # Act
     plateText, plateScore = recognitionPlateService.GetTextPlateFromImage(image)
@@ -18,11 +19,12 @@ def testGetTextPlateOldFromImage():
     assert plateText == 'LPM9710'
     assert plateScore > 0.07
 
-def testGetTextPlateNewFromImage():
+
+@pytest.mark.parametrize("recognitionPlateService", [RecognitionPlateService()])
+def testGetTextPlateNewFromImage(recognitionPlateService):
     # Arrange
     directoryImage = os.path.join(parentDirectoryTest, 'UtilsTests', 'Photos', 'CarWithNewPlate.jpg')
     image = cv2.imread(directoryImage)
-    recognitionPlateService = RecognitionPlateService()
 
     # Act
     plateText, plateScore = recognitionPlateService.GetTextPlateFromImage(image)
@@ -30,3 +32,17 @@ def testGetTextPlateNewFromImage():
     # Assert
     assert plateText == 'BEE4R22'
     assert plateScore > 0.64
+
+
+@pytest.mark.parametrize("recognitionPlateService", [RecognitionPlateService()])
+def testGetNoneTextFromImageWithPlateNotValid(recognitionPlateService):
+    # Arrange
+    directoryImage = os.path.join(parentDirectoryTest, 'UtilsTests', 'Photos', 'CarWithPlateNotValid.jpg')
+    image = cv2.imread(directoryImage)
+
+    # Act
+    plateText, plateScore = recognitionPlateService.GetTextPlateFromImage(image)
+
+    # Assert
+    assert plateText is None
+    assert plateScore is None
