b32b2e81c215a63cad4f181971d20f582af75814
Manoel Sousa <manoelvsousa1@gmail.com>
2024-03-03 18:18:12 -0300

    feat: update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+sudo apt install python3-pip
+sudo apt-get install gcc python3-dev
+sudo apt-get update
+sudo apt-get install libgl1-mesa-glx
 pip3 install virtualenv
 virtualenv -p python3 venv;
 . venv/bin/activate;
