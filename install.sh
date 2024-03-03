d9bdbe74a163b52a7bf09612895f50bd3426a279
Manoel Sousa <manoelvsousa1@gmail.com>
2024-03-03 18:19:35 -0300

    feat: update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
-sudo apt install python3-pip
-sudo apt-get update
-sudo apt-get install gcc python3-dev
-sudo apt-get update
-sudo apt-get install libgl1-mesa-glx
-pip3 install virtualenv
+sudo apt install python3-pip;
+sudo apt-get update;
+sudo apt-get install gcc python3-dev;
+sudo apt-get update;
+sudo apt-get install libgl1-mesa-glx;
+pip3 install virtualenv;
 virtualenv -p python3 venv;
 . venv/bin/activate;
 pip3 install -r requirements.txt;
 pre-commit install;
+sudo apt-get update;
