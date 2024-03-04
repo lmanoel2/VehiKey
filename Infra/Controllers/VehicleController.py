af3a9d8d90aaf5039574155fb98ba1318a77fcac
Manoel Sousa <manoelvsousa1@gmail.com>
2024-03-04 20:15:26 -0300

    feat: added url preffix to endpoint

--- Infra/Controllers/VehicleController.py
+++ Infra/Controllers/VehicleController.py
@@ -1,12 +1,11 @@
 from flask import Blueprint
 from Infra.Controllers.CRUDController import CRUDController
 
-vehicleBlueprint = Blueprint('vehicle', __name__)
-
+vehicleBlueprint = Blueprint('vehicle', __name__, url_prefix='/vehicle')
 
 class VehicleController(CRUDController):
 
-    @vehicleBlueprint.route('/testeVehicle')
+    @vehicleBlueprint.route('/')
     @staticmethod
     def read():
         return CRUDController.read()
