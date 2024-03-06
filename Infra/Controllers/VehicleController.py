8624d905323d7e972e73a233987b54367273288a
Manoel Sousa <manoelvsousa1@gmail.com>
2024-03-06 18:43:29 -0300

    feat: added again api

--- Infra/Controllers/VehicleController.py
+++ Infra/Controllers/VehicleController.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 from Infra.Controllers.CRUDController import CRUDController
+from Domain.DataBase.Context import Session
+from Domain.Entities.Vehicle import Vehicle
 
 vehicleBlueprint = Blueprint('vehicle', __name__, url_prefix='/vehicle')
 
@@ -8,4 +10,20 @@ class VehicleController(CRUDController):
     @vehicleBlueprint.route('/')
     @staticmethod
     def read():
+        session = Session()
+
+        try:
+            red_vehicles = session.query(Vehicle).filter(Vehicle.color == 'vermelha').all()
+
+            # Itere sobre os resultados, se necessário
+            for vehicle in red_vehicles:
+                return vehicle.model
+
+        except Exception as e:
+            print("Erro ao consultar o banco de dados utilizando SQL Raw:", e)
+
+        finally:
+            # Feche a sessão
+            session.close()
+
         return CRUDController.read()
--- Startup.py
+++ Startup.py
@@ -4,15 +4,19 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 project_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
 sys.path.append(project_dir)
 
-
+from flask import Flask
+from Infra.Configs.Startup.StartupConfig import RegisterBlueprints
 from Infra.Configs.Startup.DatabaseStartup import CreateOrCheckDatabase
 
 
 print("Starting...")
+app = Flask(__name__)
 
 CreateOrCheckDatabase()
+RegisterBlueprints(app)
 
-
+if __name__ == '__main__':
+    app.run(debug=False)
 
 
 
