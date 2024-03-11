ceab8fdd9bce111877f1a82ffc4c8dbcc73002eb
Manoel Sousa <manoelvsousa1@gmail.com>
2024-03-11 20:07:57 -0300

    fix: removed method not used

--- Infra/Controllers/VehicleController.py
+++ Infra/Controllers/VehicleController.py
@@ -34,24 +34,3 @@ class VehicleController(CRUDController):
             return BaseController.ReturnHttpNotAcceptable(ExceptionView(e))
         except Exception as e:
             return BaseController.ReturnHttpBadRequest(ExceptionView(e))
-
-    @staticmethod
-    @vehicleBlueprint.route('/')
-    def Get():
-        session = Session()
-
-        try:
-            red_vehicles = session.query(Vehicle).filter(Vehicle.color == 'vermelha').all()
-
-            # Itere sobre os resultados, se necessário
-            for vehicle in red_vehicles:
-                return vehicle.model
-
-        except Exception as e:
-            print("Erro ao consultar o banco de dados utilizando SQL Raw:", e)
-
-        finally:
-            # Feche a sessão
-            session.close()
-
-        return CRUDController.read()
