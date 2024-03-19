271be74cf0a3db118ad3464954395c5ba25c3ed0
Manoel Sousa <manoelvsousa1@gmail.com>
2024-03-19 18:36:42 -0300

    feat: added enumerators to event

---
+++ Domain/Enumerators/Events/Cmd.py
@@ -0,0 +1,4 @@
+from enum import Enum
+
+class Cmd(Enum):
+    ACCESS_VEHICLE = 'access_vehicle'
---
+++ Domain/Enumerators/Events/Code.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class Code(Enum):
+    ACCESS_ALLOWED = 0,
+    ACCESS_NOT_FOUND_VEHICLE = 1,
