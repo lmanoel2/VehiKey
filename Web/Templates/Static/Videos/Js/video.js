f218fc7042072dc0d2efdb21b4535aa346b40f43
Manoel Sousa <manoelvsousa1@gmail.com>
2024-04-09 20:21:44 -0300

    feat: select camera to stream video

--- Web/Templates/Static/Videos/Js/video.js
+++ Web/Templates/Static/Videos/Js/video.js
@@ -1,37 +1,3 @@
-function display_form(tipo){
-    const addVehicle = document.getElementById('add-vehicle')
-    const attVehicle = document.getElementById('att_vehicle')
-
-    if (tipo === "1"){
-        attVehicle.style.display = "none"
-        attVehicle.style.display = "none"
-        addVehicle.style.display = "block"
-    } else if(tipo === "2"){
-        attVehicle.style.display = "block"
-        addVehicle.style.display = "none"
-    }
-}
-
-function vehicle_data(){
-    const vehicle = document.getElementById("vehicle-select")
-    const csrf_token = document.querySelector('[name=csrfmiddlewaretoken]')
-    let data = new FormData()
-
-    data.append('id_vehicle', vehicle.value)
-
-    fetch('get_vehicle/', {
-        method: "POST",
-        headers: {
-          'X-CSRFToken': csrf_token.value
-        },
-        body: data
-    }).then(function (result){
-        return result.json()
-    }).then(function (data){
-        console.log(data)
-        document.getElementById("form-att-vehicle").style.display = 'block'
-        document.getElementById("btn-edit-vehicles").style.display = 'block'
-        document.getElementById('plate').value = data['plate']
-        document.getElementById('color').value = data['color']
-    })
+function submitForm() {
+    document.getElementById("camera-form").submit();
 }
--- Web/Video/views.py
+++ Web/Video/views.py
@@ -3,15 +3,22 @@ from django.views.decorators import gzip
 from django.http import StreamingHttpResponse, HttpResponse, JsonResponse
 from django.shortcuts import render
 
+from Web.Endpoints.CameraEndpoints import CameraEndpoints
 from Web.Video.Services.Video.CameraService import CameraService
 
 cameraService = CameraService()
-
+cameraEndpoints = CameraEndpoints()
+cameras = cameraEndpoints.GetAllCameras()
 
 def video(request):
     if request.method == 'GET':
         cameraService.activated = False
-        return render(request, 'video.html')
+        return render(request, 'video.html', {'cameras': cameras})
+    if request.method == 'POST':
+        cameraService.activated = False
+        camera_id = request.POST.get('camera_id')
+        print(camera_id)
+        return render(request, 'video.html', {'cameras': cameras})
 
 
 def keep_alive(request):
